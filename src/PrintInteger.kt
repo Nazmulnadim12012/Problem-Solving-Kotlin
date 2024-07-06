1358383f6144adc0a2468ccaefff04b404d36026
Nazmul Hasan Nadim <nazmulnadim12012@gmail.com>
2024-07-06 10:55:52 +0600

    1st

---
+++ src/PrintInteger.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() {
+
+    val reader = Scanner (System.`in`)
+
+    println("Input a number :")
+
+    val number = reader.nextInt()
+    println("The number is $number")
+
+}
